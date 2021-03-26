1251e8cca2f895cb2a02ab8aa48dba96f157e5b5
Jack Kellett <jackkellett02@gmail.com>
2021-03-26 20:32:04 +0000

    Movement and start of look

---
+++ Perlin Noise Type Game/Assets/Prefabs/FP Controller/LookScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LookScript : MonoBehaviour {
+	#region Variables to assign via the unity inspector (Serialize Fields)
+	[SerializeField]
+	private GameObject playerCamera;
+
+	[SerializeField]
+	private float lookSensitivity = 10.0f;
+	#endregion
+
+	#region Variable Declarations
+	private Vector2 lookVector = Vector2.zero;
+	#endregion
+
+	#region Private Functions
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	private void HorizontalRotation() {
+		Vector3 playerRotation = gameObject.transform.rotation.eulerAngles;
+		if(lookVector.x > 0.0f) {
+			playerRotation.y += lookSensitivity * Time.deltaTime;
+		} else if(lookVector.x < 0.0f) {
+			playerRotation.x -= lookSensitivity * Time.deltaTime;
+		}
+	}
+
+	private void OnLook(InputValue value) {
+		Debug.Log("Looking!!");
+		lookVector = value.Get<Vector2>();
+	}
+	#endregion
+
+	#region Public Access Functions (Getters and Setters)
+
+	#endregion
+}
---
+++ Perlin Noise Type Game/Assets/Prefabs/FP Controller/PlayerMovementScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour {
+	#region Variables to assign via the unity inspector (Serialize Fields)
+	[SerializeField]
+	private LayerMask groundLayer;
+
+	[SerializeField]
+	private float movementSpeed = 10.0f;
+
+	[SerializeField]
+	private float gravityValue = 9.81f;
+	#endregion
+
+	#region Variable Declarations
+	private CharacterController playerController;
+	private Vector3 playerVelocity = Vector3.zero;
+	private Vector2 i_Movement = Vector2.zero;
+	private bool onGround = false;
+	#endregion
+
+	#region Private Functions
+	// Start is called before the first frame update
+	void Start() {
+		playerController = gameObject.GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		onGround = CheckIfOnGround();
+		Move();
+	}
+
+	private bool CheckIfOnGround() {
+		Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, 0.5f, groundLayer);
+		if(colliders.Length > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void Move() {
+		playerVelocity.y -= gravityValue * Time.deltaTime;
+		if (onGround) {
+			playerVelocity.y = 0.0f;
+		}
+
+		playerVelocity.x = i_Movement.x;
+		playerVelocity.z = i_Movement.y;
+		playerController.Move(playerVelocity);
+	}
+
+	private void OnMove(InputValue value) {
+		Debug.Log("Moving!!");
+		i_Movement = value.Get<Vector2>() * movementSpeed * Time.deltaTime;
+	}
+	#endregion
+
+	#region Public Access Functions (Getters and setters)
+
+	#endregion
+}
